02f039285855a0ef6eadadbf4bb1ded21e56f2c3
kizzze <111867526+zvorsiesz@users.noreply.github.com>
2023-03-05 22:38:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const {Client, SpotifyRPC, CustomStatus, RichPresence} = require('discord.js-selfbot-v13');
 const delay = require('delay')
 const fs = require('fs');
-// инициализация 
+// Инициализация 
 const client = new Client();
 const start_time = Math.floor(Date.now() / 1000)
 function randint(min, max) { 
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
-console.log('Статус пошел!')
+console.log('Status starting!')
 client.on('ready', async () => {
     var end_mins = 0
     var end_hrs = 0
@@ -16,19 +16,18 @@ client.on('ready', async () => {
     .setState("текст") // Пользовательский статус (из дискорд клиента)
     
     const r = new RichPresence(client)
-	.setAssetsLargeImage("mp: attachments/1077987747228287026/1079143354928988190/444c8716cea9a6065cda840240d4db14.gif") // Картинка
-	.setAssetsLargeText('Количество заявок в друзья обновляется раз в 30с') // Текст при наводке
+	.setAssetsLargeImage("mp: attachments/1077987747228287026/1079143354928988190/444c8716cea9a6065cda840240d4db14.gif") // Гиф
+	.setAssetsLargeText('') // Текст 
     .setType("STREAMING") 
-    .setURL("") //ссылка, например https://www.twitch.tv/kizzzexc
-    .setApplicationId(234395307759108106) // Можешь не менять, это Groovy
-    .addButton("название", "ссылка") // Кнопки
-    .addButton("название","ссылка") // Кнопки
+    .setURL("link") //ссылка, например https://www.twitch.tv/kizzzexc
+    .setApplicationId("ApplicationId") // Айди Апликации
+    .addButton("текст", "ссылка") // Кнопки
+    .addButton("текст", "ссылка") // Кнопки
     .setName("Twitch") // Строка где "Стримит на <название>"
-	.setDetails('?') // Белый текст сверху
+	.setDetails('текст') // Белый текст сверху
     
     while(true) {
        let relc = client.relationships.incomingCache.size
-       r.setState(`Заявок в друзья ${relc}`)/ // Счетчик заявок в друзья (бонус тип)
 		// если нет необходимости, закоментируй эту часть кода "/* */"
      client.user.setPresence({activities: [r.toJSON(), customactivity.toJSON()]})
         await delay(30000)
@@ -36,4 +35,4 @@ client.on('ready', async () => {
 
 
 // Соединение токена
-client.login('Token'); // Токен, ключ от аккаунта
+client.login('Token'); // Токен (ключ от аккаунта)
